83a8b85f5867674faafc8c9d945d40ff6d848409
Shubham Agarwal <shubhamrocks0709@gmail.com>
2023-06-09 14:24:25 +1000

    unused import removed

--- dark-mode/src/routes/App/components/App.js
+++ dark-mode/src/routes/App/components/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 
